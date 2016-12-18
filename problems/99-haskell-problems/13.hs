bc64e81b567a7f6e118f43e5edddf5d88f41e278
Aditya K <adityavkk@gmail.com>
2016-12-18 17:37:04 -0500

    completel 13 from 99-haskell

---
+++ problems/99-haskell-problems/13.hs
@@ -0,0 +1,17 @@
+data OneOrMany e = None
+                 | One e
+                 | Many Int e
+  deriving Show
+
+encode :: (Eq a) => [a] -> [OneOrMany a]
+encode [] = []
+encode (x:xs) = oneOrMany x n : encode (drop n xs)
+  where
+    n = numOf x xs
+    numOf y [] = 0
+    numOf y (x:xs)
+      | x /= y = 0
+      | otherwise = 1 + numOf y xs
+    oneOrMany x n
+      | n == 0 = One x
+      | otherwise = Many (n + 1) x
