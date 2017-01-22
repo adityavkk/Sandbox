be96191a06783faf1c3af5fa2fc5b4a8ef80b390
Aditya K <adityavkk@gmail.com>
2017-01-21 23:10:10 -0500

    fix lispyLists by adding the foldR import

--- data-structures/functional/lispyLists.js
+++ data-structures/functional/lispyLists.js
@@ -1,4 +1,8 @@
-/* a purely functional, lisp like linked list implementation with delete */
+/* A purely functional, lisp like linked list implementation with delete
+ * There are no pointers, or node objects etc. in this list implementation,
+ * */
+
+const { foldR, log } = require('../../utils/utils')
 
 function cons(h, t) {
   return (a) => {
@@ -26,7 +30,7 @@ function uncons(n, ll) {
 }
 
 function list(...xs) {
-  return foldRight(cons, null, xs)
+  return foldR(cons, null, xs)
 }
 
 // printList :: [a] -> String
@@ -34,3 +38,7 @@ function printList(ll) {
   if (ll === null) return 'null'
   return car(ll) + ' ' + printList(cdr(ll))
 }
+
+const xs = list(1, 2, 3, 4)
+log(printList(xs)) // 1 2 3 4 null
+log(cadr(xs)) // 2
