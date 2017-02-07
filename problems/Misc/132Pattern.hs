c9c067f4e1658fa0bdd4452ae0abf6b1cbe625d5
Aditya K <adityavkk@gmail.com>
2017-02-06 23:27:54 -0500

    complete 132Pattern in hs

---
+++ problems/Misc/132Pattern.hs
@@ -0,0 +1,24 @@
+-- given a list xs return if 3 elements in the list satisfy the 132 condition
+-- i.e. for some i < j < k, there exist xi < xk < xj
+-- [1,2,3,4] -> False
+-- [3,1,4,2] -> True
+
+trd (_, _, a) = a
+
+f :: [Int] -> Bool
+f ns = snd $ foldl g ([], False) $ reverse ns
+  where
+    g ([], r) x = ([x], r)
+    g ([y], r) x 
+      | x > y = ([x, y], r)
+      | otherwise = ([y], r)
+    g (xs@(y:z:_), r) x
+      | r || x < z = (xs, True)
+      | x > y      = (x:xs, r)
+      | otherwise  = (xs, r)
+
+
+xs = [1,2,3,4] :: [Int]
+ys = [3,1,4,2] :: [Int]
+zs = [ 9, 11, 8, 9, 10, 7, 9 ] :: [Int]
+as = [1,0,1,-4,-3] :: [Int]
--- problems/Misc/132Pattern.js
+++ problems/Misc/132Pattern.js
@@ -1,26 +1,18 @@
-function ottPattern(xs) {
-  if (xs.length < 3) return false;
-  for (let i = xs.length - 1; i > 1; i--) {
-    const xi = xs[i];
-    if (largeThenSmall(xs.slice(0, i), xi)) return true;
-  }
-  return false;
-}
-function largeThenSmall(ns, x) {
-  let lastN = ns[ns.length - 1], withoutLast = ns.slice(0, ns.length - 1);
-  if (ns.length < 2) return false;
-  if (lastN > x) {
-    return smaller(withoutLast, x)
-  }
-  return largeThenSmall(withoutLast, x)
+const f = xs => {
+  const t = xs.slice(0, 3),
+    rest = xs.slice(3),
+    s = Math.min(...t),
+    m = Math.max(...t)
+  return rest.reduce((z, x) => {
+    const [s, m, r] = z
+    if (r) return z
+    if (x < m && x > s) return [s, m, true]
+    if (x < s) return [x, m, r]
+    if (x > m) return [s, x, r]
+    if (x == m || x == s) return z
+  }, [s, m, false])[2]
 }
 
-function smaller (ns, x) {
-  return Math.min(...ns) < x
-}
-
-function canAccommodate (memo, ns, x) {
-
-}
-
-console.log(ottPattern([7, 9, 3, 4, 8, 12]))
+console.log(f([9, 11, 8, 9, 10, 7, 9]))
+console.log(f([1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 10]))
+console.log(f([1,0,1,-4,-3]))
