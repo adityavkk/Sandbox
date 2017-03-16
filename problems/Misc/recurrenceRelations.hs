c1d4905a76b75a85907fdf162f1b411cab391429
Aditya K <adityavkk@gmail.com>
2017-03-15 23:53:33 -0400

    complete recurrenceRelations DSL with memoization using the State Monad

--- problems/Misc/recurrenceRelations.hs
+++ problems/Misc/recurrenceRelations.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
+
 import qualified Data.HashMap.Lazy as H
 import Data.Either
 import Data.Hashable
-import qualified Data.Map as M
 import Control.Monad
 import Control.Monad.State
 
@@ -23,36 +23,31 @@ coinchange (a, i)   | a == 0          = Left 1
                     | otherwise       = Right ([(a, i-1), (a-coinlist!!(i-1), i)], sum)
 coinlist = [1, 3, 5, 10]
 
-{- evaluateFunction :: (Num a, Eq a, Enum a, Hashable a) => (a -> Either b ([a], [b] -> b)) -> a -> b -}
-{- evaluateFunction f n = f' n -}
-  {- where -}
-    {- memo    = H.fromList [(n', g $ f' <$> xs) | n' <- [0..]] -}
-    {- (xs, g) = fromRight $ f n -}
-    {- -- table n = H.insert n (g <$> f' <$> xs) memo -}
-    {- f' n -}
-      {- | isLeft $ f n = fromLeft $ f n -}
-      {- | otherwise    = memo H.! n -}
-
-{- evaluateFunction' :: Ord a => (a -> Either b ([a], [b] -> b)) -> a -> b -}
-{- evaluateFunction' f n = case f n of -}
-                         {- (Left x)        -> x -}
-                         {- (Right (xs, g)) -> g $ evaluateFunction' f <$> xs -}
-fib = f'
+evaluateFunction :: (Ord a, Hashable a) =>
+  (a -> Either b ([a], [b] -> b))
+  -> a -> b
+evaluateFunction f n = evalState (cache f' n) H.empty
   where
-    memo = map f' [0..]
-    f n = memo !! n
-    f' n | n < 2 = 2
-         | otherwise = f (n - 1) + f (n - 2)
+    f' a = case f a of
+             Left v           -> return v
+             Right (xs, oper) -> oper <$> sequence (cache f' <$> xs)
 
-memoized f n = do maybeCached <- liftM (M.lookup n) get
-                  case maybeCached of
-                    Just value -> return value
-                    Nothing    -> do result <- f n
-                                     modify (M.insert n result)
-                                     return result
+cache :: (MonadState (H.HashMap k v) m, Ord k, Hashable k) => 
+  (k -> m v) ->
+  k ->
+  m v
+cache g n = do
+  memo <- H.lookup n <$> get
+  case memo of
+    Just v -> return v
+    _      -> do
+                r <- g n
+                modify (H.insert n r)
+                return r
 
-evaluateFunction f n =
-    evalState (memoized evalInner n) M.empty
-    where evalInner n = case f n of
-                            Left value            -> return value
-                            Right (list, combine) -> liftM combine (mapM (memoized evalInner) list)
+
+-- Check state for key, n, if found return it in state monad
+-- Otherwise, call evalInner with key, n, which then calls f n
+-- If f n is Left val -> return val in state monad
+  -- else combine mapping (memoized evalInner) onto the list from Right
+-- Modify state with the result stored with key
