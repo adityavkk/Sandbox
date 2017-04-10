81f8759c8c9236ffde9eea56ab152feffed51b96
Aditya K <adityavkk@gmail.com>
2017-04-10 11:21:09 -0400

    update smallest sequence with given primes

---
+++ problems/Misc/trees-and-graphs/smallestSequenceWithGivenPrimes.js
@@ -0,0 +1,8 @@
+/* Given three prime numbers (p1, p2, p3) and an integer k. Find the first k integers
+ * which only have p1, p2, and p3 or a combination of them as prime factors
+ *
+ * ps = [2, 3, 5] k = 5
+ * output: [2, 3, 4, 5, 6]
+ */
+
+/*
