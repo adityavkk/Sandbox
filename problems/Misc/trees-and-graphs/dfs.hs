1d4e474e1ab30d46df350f83dd57e230ed04d895
Aditya K <adityavkk@gmail.com>
2017-05-01 10:02:41 -0400

    complete dfs in hs

--- problems/Misc/trees-and-graphs/dfs.hs
+++ problems/Misc/trees-and-graphs/dfs.hs
@@ -4,15 +4,15 @@ import qualified Data.HashSet as S
 type AL = H.HashMap Int [Int]
 
 g :: AL
-g = H.fromList [(1, [2, 3]), (2, [4, 3]), (3, [4, 1]), (4, [3])]
+g = H.fromList [(1, [2, 3]), (2, [4, 5]), (3, [6, 7]), (4, []), (5, []), (6, []), (7, [])]
 
 g' :: AL
 g' = H.fromList [(1, [3]), (3, [])]
 
-dfs g = f (H.keys g) S.empty
+dfs g = fst $ f (H.keys g) S.empty
   where
-    f vs s = foldr (\ v (xs, s') -> if S.member v s'
+    f vs s = foldl (\ (xs, s') v -> if S.member v s'
                                     then (xs, s')
-                                    else let (ys, s'') = f (g H.! v) (S.insert v s) in
+                                    else let (ys, s'') = f (g H.! v) (S.insert v s') in
                                              (xs ++ (v:ys), S.union s'' s'))
-                         ([], S.empty) vs
+                         ([], s) vs
