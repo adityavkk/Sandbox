12c4efef0e10990e57898674d7d12a5dae03aeb7
Aditya Kumarakrishnan <adityavkk@gmail.com>
2017-07-09 01:41:47 -0400

    complete 6 from 99-fsharp problems; add module structure to 5

--- problems/99-fsharp-problems/P5.fsx
+++ problems/99-fsharp-problems/P5.fsx
@@ -1,3 +1,4 @@
 // Reverse a list
+module P5
 
 let rev xs = List.fold (fun ys x -> x :: ys) [] xs
---
+++ problems/99-fsharp-problems/P6.fsx
@@ -0,0 +1,5 @@
+// Find out whether a list is a palindrome
+
+#load "P5.fsx"
+
+let isPalindrome xs = P5.rev xs = xs
