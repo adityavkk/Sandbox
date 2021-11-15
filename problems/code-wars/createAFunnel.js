feb7c051354d3cf1f4fdb0aa303fca803cf0e5ea
Aditya Kumarakrishnan <adityavkk@gmail.com>
2021-11-15 18:33:30 -0500

    update create funnel

--- problems/code-wars/createAFunnel.js
+++ problems/code-wars/createAFunnel.js
@@ -1,10 +1,11 @@
+const vm = require('vm')
+const util = require('util')
 /*
- *   \         /
-   		\7 8 9  /
-    	 \4 5 6/
-     	  \2 3/
-      	 \1/
- *
+ *   \1 _ 3 4  / 5
+   		\1 9 8 9/ 4
+    	 \7 5 6/ 3
+     	  \9 3/ 2 
+      	 \2/ 1 
  */
 
 class Vertex {
@@ -19,26 +20,26 @@ class Vertex {
 }
 
 class DirectedGraph {
-  adjacencyList = {}
+  adjacencyList = new Map()
   root = null
 
   constructor () {}
 
   addEdge(v1, v2) {
-    const isV1InGraph = v1 in this.adjacencyList
-    const isV2InGraph = v2 in this.adjacencyList
-    if (!isV1InGraph) this.adjacencyList[v1] = new Set()
-    if (!isV2InGraph) this.adjacencyList[v2] = new Set()
-    this.adjacencyList[v1].add(v2)
+    const isV1InGraph = this.adjacencyList.has(v1)
+    const isV2InGraph = this.adjacencyList.has(v2)
+    if (!isV1InGraph) this.adjacencyList.set(v1, new Set())
+    if (!isV2InGraph) this.adjacencyList.set(v2,new Set())
+    this.adjacencyList.get(v1).add(v2)
   }
 
   addVertex(v) {
     const isVInGraph = v in this.adjacencyList
-    if (!isVInGraph) this.adjacencyList[v] = new Set()
+    if (!isVInGraph) this.adjacencyList.set(v, new Set())
   }
 
   neighbours(v) {
-    return this.adjacencyList[v]
+    return this.adjacencyList.get(v)
   }
 
   set root(v) {
@@ -46,23 +47,28 @@ class DirectedGraph {
   }
 
   get vertices() {
-    return Object.keys(this.adjacencyList)
+    return this.adjacencyList.keys()
   }
 
   weightAbove(vertex) {
     return dfs(vertex, this)
+  }
+
+  removeEdge(v1, v2) {
+    this.neighbours(v1).delete(v2)
   }
 }
 
 const dfs = (vertex, graph) => {
   const visited = new Set()
   const go = vertex => {
-    visited.add(vertex)
     const neighbours = graph.neighbours(vertex)
     if (neighbours.size == 0) return 0
+
     const unvisitedNeighbours = Array.from(neighbours).filter(v => !visited.has(v))
-    console.log(vertex.val, unvisitedNeighbours)
-    return 1 + unvisitedNeighbours.reduce((sum, v) => sum + go(v), 0)
+    unvisitedNeighbours.forEach(v => visited.add(v))
+
+    return unvisitedNeighbours.length + unvisitedNeighbours.reduce((sum, v) => sum + go(v), 0)
   }
   return go(vertex)
 }
@@ -71,35 +77,121 @@ class Funnel {
 
   #funnel = new DirectedGraph()
 
-  constructor() {
-    const root = new Vertex(1)
-    this.#funnel.addVertex(root)
-    this.#funnel.root = root
+  constructor(funnel) {
+    if(funnel) this.#funnel = funnel
   }
 
   fill(...xs) {
-    
+    //create vertices for xs
+    //add them to the bottom level of the graph 
+    //update neighbours (at most 2 vertices)
   }
   
-  drip() {}
+  drip() {
+    const graph = this.#funnel
+    const getHeaviestNeighbour = vertex => {
+      const neighbours = graph.neighbours(vertex)
+      if (neighbours.size == 0) return null
+      if (neighbours.size == 1) return Array.from(neighbours)[0]
+      const [leftWeight, rightWeight] = Array.from(neighbours).map(n => graph.weightAbove(n))
+      const heaviestNeighbour = leftWeight > rightWeight ? neighbours[0] : neighbours[1]
+      return heaviestNeighbour
+    }
+    // return the heaviest neighbour and restore the graph edges
+    const replaceWithHeaviestNeighbour = vertex => {
+      // remove edge between heaviest neighbour and vertex
+      // add an edge between the other neighbour of vertex and heaviest
+      // recursively replaceWithHeaviestNeighbour on heaviest neighbour
+      const graph = this.#funnel
+      const neighboursOfVertex = graph.neighbours(vertex)
+      const heaviest = getHeaviestNeighbour(vertex)
+      console.log(neighboursOfVertex)
+      const otherNeighbours = Array.from(neighboursOfVertex).filter(v => v !== heaviest)
+      graph.removeEdge(vertex, heaviest)
+      otherNeighbours.forEach(n => graph.addEdge(heaviest, n))
+      replaceWithHeaviestNeighbour(heaviest)
+      return heaviest
+    }
 
-  toString() {
-    return `\\${this.#funnel.root.val}/`
+    graph.root = replaceWithHeaviestNeighbour(graph.root)
+
+  }
+
+  draw() {
+    // BFS and stringify each level
+    const graph = this.#funnel
+    const bfsOrder = bfs(graph.root, graph)
+    const numElements = [1, 2, 3, 4, 5]
+    const levels = numElements.reduce((levels, n) => {
+      levels.push(bfsOrder.splice(0, n))
+      return levels
+    }, []).reverse()
+    return levels.map((l, i) => `${spaces(i)}\\${l.join(' ')}/`).join('\n')
   }
 }
 
+const spaces = i => new Array(i).fill(' ').join('')
+const draw = (graph) => {
+}
+
+const bfs = (vertex, graph) => {
+  // Q = [vertex]
+  // loop:
+    // v = deQ 
+    // do work
+    // add childen of v to Q
+  const order = []
+  const Q = [vertex]
+  const visited = new Set()
+  while (Q.length > 0) {
+    const v = Q.shift()
+    if (visited.has(v)) continue
+    order.push(v.val)
+    visited.add(v)
+    const neighbours = graph.neighbours(v)
+    Q.push(...neighbours)
+  }
+  return order
+}
+
 const g = new DirectedGraph()
-const v1 = new Vertex(1)
-const v2 = new Vertex(2)
-const v3 = new Vertex(3)
-const v4 = new Vertex(4)
+const range = n => new Array(n).fill(0).map((_, i) => i+1)
+const sandbox = vm.createContext({})
 
-g.addEdge(v1, v2)
-g.addEdge(v1, v3)
-g.addEdge(v2, v4)
+const setUpVertices = (n) => {
+  const nRange = range(n)
+  const vNames = nRange.map(i => `v${i}`) 
+  const rand = () => Math.floor(Math.random() * 9) + 1
+  const vertices = nRange.map(i => new Vertex(rand()))
+  vNames.forEach((v, i) => {
+    vm.runInContext(`${v}=${vertices[i]}`, sandbox)
+  })
+}
+setUpVertices(15)
+g.addEdge(sandbox.v1, sandbox.v2)
+g.addEdge(sandbox.v1, sandbox.v3)
+g.addEdge(sandbox.v2, sandbox.v4)
+g.addEdge(sandbox.v2, sandbox.v5)
+g.addEdge(sandbox.v3, sandbox.v5)
+g.addEdge(sandbox.v3, sandbox.v6)
+g.addEdge(sandbox.v4, sandbox.v7)
+g.addEdge(sandbox.v4, sandbox.v8)
+g.addEdge(sandbox.v5, sandbox.v8)
+g.addEdge(sandbox.v5, sandbox.v9)
+g.addEdge(sandbox.v6, sandbox.v9)
+g.addEdge(sandbox.v6, sandbox.v10)
+g.addEdge(sandbox.v7, sandbox.v11)
+g.addEdge(sandbox.v7, sandbox.v12)
+g.addEdge(sandbox.v8, sandbox.v12)
+g.addEdge(sandbox.v8, sandbox.v13)
+g.addEdge(sandbox.v9, sandbox.v13)
+g.addEdge(sandbox.v9, sandbox.v14)
+g.addEdge(sandbox.v10, sandbox.v14)
+g.addEdge(sandbox.v10, sandbox.v15)
+g.root = sandbox.v1
 
+const foo = new Funnel(g)
+console.log(foo.draw())
+console.log(foo.drip())
+console.log(foo.draw())
 
-const foo = new Funnel()
-console.log(foo.toString())
-console.log(g.vertices)
-console.log(g.weightAbove(v1))
