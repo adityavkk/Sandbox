8619d7deb34b32671be139c4f7651954f7470a7c
Aditya Kumarakrishnan <adityavkk@gmail.com>
2021-11-16 09:35:25 -0500

    update createAFunnel

--- problems/code-wars/createAFunnel.js
+++ problems/code-wars/createAFunnel.js
@@ -51,26 +51,12 @@ class DirectedGraph {
   }
 
   weightAbove(vertex) {
-    return dfs(vertex, this)
+    return Math.max(0, bfs(vertex, this).length - 1)
   }
 
   removeEdge(v1, v2) {
     this.neighbours(v1).delete(v2)
   }
-}
-
-const dfs = (vertex, graph) => {
-  const visited = new Set()
-  const go = vertex => {
-    const neighbours = graph.neighbours(vertex)
-    if (neighbours.size == 0) return 0
-
-    const unvisitedNeighbours = Array.from(neighbours).filter(v => !visited.has(v))
-    unvisitedNeighbours.forEach(v => visited.add(v))
-
-    return unvisitedNeighbours.length + unvisitedNeighbours.reduce((sum, v) => sum + go(v), 0)
-  }
-  return go(vertex)
 }
 
 class Funnel {
@@ -88,13 +74,16 @@ class Funnel {
   }
   
   drip() {
+    console.log(`DRIPPING`)
     const graph = this.#funnel
     const getHeaviestNeighbour = vertex => {
-      const neighbours = graph.neighbours(vertex)
-      if (neighbours.size == 0) return null
-      if (neighbours.size == 1) return Array.from(neighbours)[0]
-      const [leftWeight, rightWeight] = Array.from(neighbours).map(n => graph.weightAbove(n))
-      const heaviestNeighbour = leftWeight > rightWeight ? neighbours[0] : neighbours[1]
+      const neighbours = Array.from(graph.neighbours(vertex))
+      console.log(vertex, neighbours)
+      if (neighbours.length == 0) return null
+      if (neighbours.length == 1) return neighbours[0]
+      const [leftWeight, rightWeight] = neighbours.map(n => graph.weightAbove(n))
+      const heaviestNeighbour = leftWeight >= rightWeight ? neighbours[0] : neighbours[1]
+      //console.log(`LEFT`, leftWeight, `RIGHT`, rightWeight, neighbours, heaviestNeighbour)
       return heaviestNeighbour
     }
     // return the heaviest neighbour and restore the graph edges
@@ -102,23 +91,27 @@ class Funnel {
       // remove edge between heaviest neighbour and vertex
       // add an edge between the other neighbour of vertex and heaviest
       // recursively replaceWithHeaviestNeighbour on heaviest neighbour
-      const graph = this.#funnel
-      const neighboursOfVertex = graph.neighbours(vertex)
       const heaviest = getHeaviestNeighbour(vertex)
-      console.log(neighboursOfVertex)
-      const otherNeighbours = Array.from(neighboursOfVertex).filter(v => v !== heaviest)
-      graph.removeEdge(vertex, heaviest)
-      otherNeighbours.forEach(n => graph.addEdge(heaviest, n))
-      replaceWithHeaviestNeighbour(heaviest)
-      return heaviest
+      console.log(`HEAVIEST`, heaviest)
+      if (heaviest == null) return null
+      if (heaviest !== null) {
+        graph.neighbours(vertex).forEach(n => {
+          graph.removeEdge(vertex, n)
+          graph.addEdge(heaviest, n)
+        })
+        replaceWithHeaviestNeighbour(heaviest)
+        return heaviest
+      }
     }
 
-    graph.root = replaceWithHeaviestNeighbour(graph.root)
-
+    const replacedRoot = replaceWithHeaviestNeighbour(graph.root)
+    //console.log(`ROOT`, replacedRoot)
+    graph.root = replacedRoot
   }
 
   draw() {
     // BFS and stringify each level
+    console.log(`DRAWING`)
     const graph = this.#funnel
     const bfsOrder = bfs(graph.root, graph)
     const numElements = [1, 2, 3, 4, 5]
@@ -157,11 +150,11 @@ const bfs = (vertex, graph) => {
 const g = new DirectedGraph()
 const range = n => new Array(n).fill(0).map((_, i) => i+1)
 const sandbox = vm.createContext({})
+const rand = () => Math.floor(Math.random() * 9) + 1
 
 const setUpVertices = (n) => {
   const nRange = range(n)
   const vNames = nRange.map(i => `v${i}`) 
-  const rand = () => Math.floor(Math.random() * 9) + 1
   const vertices = nRange.map(i => new Vertex(rand()))
   vNames.forEach((v, i) => {
     vm.runInContext(`${v}=${vertices[i]}`, sandbox)
@@ -190,8 +183,10 @@ g.addEdge(sandbox.v10, sandbox.v14)
 g.addEdge(sandbox.v10, sandbox.v15)
 g.root = sandbox.v1
 
+
 const foo = new Funnel(g)
-console.log(foo.draw())
-console.log(foo.drip())
-console.log(foo.draw())
+
+// console.log(foo.draw())
+// console.log(foo.drip())
+// console.log(foo.draw())
 
