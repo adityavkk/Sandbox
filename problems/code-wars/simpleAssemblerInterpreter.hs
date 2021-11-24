05a59489b577dd93cdb512d08fe7eb874bdeb2d7
Aditya Kumarakrishnan <adityavkk@gmail.com>
2021-11-24 10:24:27 -0500

    finish simpleAssemplerInterpreter

--- problems/code-wars/simpleAssemblerInterpreter.hs
+++ problems/code-wars/simpleAssemblerInterpreter.hs
@@ -1,58 +1,76 @@
--- We want to create a simple interpreter of assembler which will support the following instructions:
-
--- mov x y - copies y (either a constant value or the content of a register) into register x
--- inc x - increases the content of the register x by one
--- dec x - decreases the content of the register x by one
--- jnz x y - jumps to an instruction y steps away (positive means forward, negative means backward, y can be a register or a constant), but only if x (a constant or a register) is not zero
--- Register names are alphabetical (letters only). Constants are always integers (positive or negative).
-
--- Note: the jnz instruction moves relative to itself. For example, an offset of -1 would continue at the previous instruction, while an offset of 2 would skip over the next instruction.
-
--- The function will take an input list with the sequence of the program instructions and will execute them. The program ends when there are no more instructions to execute, then it returns a dictionary with the contents of the registers.
-
--- Also, every inc/dec/jnz on a register will always be preceeded by a mov on the register first, so you don't need to worry about uninitialized registers.
-
--- Approach:
---  - tokenize each operation into a token = AST
---  - AST will be a map with key = index of the instruction to accomodate the jnz instruction
-
---  - eval index ast state 
---      - run instruction state
---          - Mov r x        = eval (index + 1), state = set r x state
---          - Inc r, Dec r   = eval (index + 1), state = inc, dec r
---          - Jnz r i        = if contents r not 0, then eval (index - i) else eval (index + 1) 
---  - eval 0 ast emptyState
+-- Simple Assembler Interpreter (https://www.codewars.com/kata/58e24788e24ddee28e000053)
 
 module SimpleAssembler (simpleAssembler) where
 import qualified Data.Map.Strict as M
+import Data.Char
 
 type Register  = Char
 type Registers = M.Map Register Int
 type AST       = M.Map Int Token
+type Instructions = [Token]
 
 data Token   = Mov Register Int
              | Inc Register
              | Dec Register
              | Jnz Register Int
+             deriving (Show)
 
-toToken ('m':'o':'v':' ':r:' ':v:_) = Mov r (digitsToInt v)
-toToken ('j':'n':'z':' ':r:' ':v:_) = Jnz r (digitsToInt v)
+toToken :: String -> Token
+toToken ('m':'o':'v':' ':r:' ':v:_) = Mov r (digitToInt v)
+toToken ('j':'n':'z':' ':r:' ':number) = Jnz r (read number)
 toToken ('i':'n':'c':' ':r:_)       = Inc r
 toToken ('d':'e':'c':' ':r:_)       = Dec r
 
-tokenize instructions = M.fromList $ zip [1..length] (map toToken instructions)
+tokenize :: [String] -> Instructions
+tokenize instructions = map toToken instructions
 
-eval ast = go 0 ast M.empty
+eval :: Instructions -> Registers
+eval instructions = go 1 ast M.empty
     where 
-        go i ast state           = run (state M.! (ast M.! i)) i state
-        run (Mov r x) i state    = go (i + 1) ast (M.insert r x state)
-        run (Inc r) i state      = go (i + 1) ast (M.insertWith (+) r 1 state)
-        run (Dec r) i state      = go (i + 1) ast (M.insertWith (-) r 1 state)
-        run (Jnz r jump) i state = if (state M.! i /= 0) then go (i + jump) ast state else go (i + 1) ast state
+      ast                      = M.fromList $ zip [1..length instructions] instructions
+      isDone i                 = i > length instructions
+      go i ast state           = if isDone i then state else run (ast M.! i) i state
+      run (Mov r x) i state    = go (i + 1) ast (M.insert r x state)
+      run (Inc r) i state      = go (i + 1) ast (M.insertWith (+) r 1 state)
+      run (Dec r) i state      = go (i + 1) ast (M.insertWith (flip (-)) r 1 state)
+      run (Jnz r jump) i state = if (state M.! r) /= 0 then go (i + jump) ast state else go (i + 1) ast state
 
 simpleAssembler :: [String] -> Registers
 simpleAssembler = eval . tokenize
 
 
--------------------- TESTS -------------------- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+---------------------------------------- TESTS ---------------------------------------- 
 case1 = ["mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a"]
+case2 = ["mov a 5", "inc a", "dec a", "mov b 1", "dec b"]
