9f2e823cc4d7825adf5c08790928b72a0ca75f3f
Aditya Kumarakrishnan <adityavkk@users.noreply.github.com>
2022-12-07 22:40:24 -0500

    Create alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.js

---
+++ problems/LeetCode/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.js
@@ -0,0 +1,41 @@
+const alertNames = (keyName, keyTime) => {
+  const visitsByName = keyName.reduce((map, name, i) => {
+    if (map[name]) {
+      map[name].push(keyTime[i])
+    }
+    else {
+      map[name] = [keyTime[i]]
+    }
+    return map
+  }, {})
+
+  const result = Object.entries(visitsByName)
+    .filter(([name, times]) => hasAlert(times))
+    .map(([name, _]) => name)
+
+    result.sort()
+    return result
+    
+}
+
+const parseTime = timeS => {
+  const [hour, min] = timeS.split(":")
+  return +hour * 60 + +min
+}
+
+const withinHour = (start, end) => { 
+  const timeDiff = parseTime(end) - parseTime(start)
+  const result = timeDiff >= 0 && timeDiff <= 60
+  return result
+}
+
+const hasAlert = (times) => {
+  times.sort()
+  const window = []
+  for(let time of times) {
+    while(window.length && !withinHour(window[0], time)) window.shift()
+    window.push(time)
+    if (window.length >= 3) return true
+  }
+  return false
+}
